09e29e5be84ae511eb77f39c8a76a333ea4c28e0
shijie <cs5129606@gmail.com>
2023-02-15 06:05:36 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { Admin, ListGuesser, Resource } from 'react-admin';
+import { authProvider } from './utils/authProvider';
+import { ThemeProvider, createTheme, Typography } from '@mui/material';
+import Fjallaone from './assets/fonts/fjallaone.woff2';
+import CssBaseline from "@mui/material/CssBaseline";
 
 function App() {
+
+  const theme = createTheme({
+    typography: {
+      fontFamily: "fjallaone"
+    },
+    components: {
+      MuiCssBaseline: {
+        styleOverrides: {
+          "@font-face": {
+            fontFamily: "fjallaone",
+            src: `url(${Fjallaone}) format("truetype")`
+          },
+          body: {
+            fontFamily: "fjallaone",
+            color: "red"
+          }
+        }
+      }
+    }
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {/* <Typography variant="h1">Hello World</Typography> */}
+      <Admin
+        authProvider={authProvider}
+        theme={theme}
+      >
+        <Resource name='tags' list={ListGuesser} />
+      </Admin>
+    </ThemeProvider>
+    // <ThemeProvider theme={theme}>
+    //    <CssBaseline />
+    //   <Admin
+    //     authProvider={authProvider}
+    //   >
+    //     <Resource name='tags' list={ListGuesser} />
+    //   </Admin>
+    // </ThemeProvider>
   );
 }
 
